47143ff4f33b186717f7afa186e19a362a0a249b
scott_tsao <o60816@gmail.com>
2019-04-03 14:43:11 +0800

    Refactor the code

--- content.js
+++ content.js
@@ -1,24 +1,13 @@
-console.log("內插腳本載入");
+console.log("Append the audio list at the right side");
 
-(function() {
-    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
-        function(callback) {
-            window.setTimeout(callback, 1000 / 60);
-        };
-    window.requestAnimationFrame = requestAnimationFrame;
-})();
-
-var audios = document.getElementsByTagName("audio");
+var audios = [].slice.call(document.getElementsByTagName("audio"));
 var audioDiv = document.createElement("div")
 function appendAudio(){
-    var length = audios.length;
-    for(var i=0;i!=length;++i){
-        var audio = document.createElement("audio");
-        audio.src = audios[i].src;
-        audio.controls = "true"
-        audioDiv.appendChild(audio);
-        audioDiv.appendChild(document.createElement("br"));
-        audioDiv.appendChild(document.createElement("br"));
+    for(var i=0;i!=audios.length;++i){
+        if(audios[i].src==undefined)
+            continue;
+        audioDiv.appendChild(audios[i]);
+        audioDiv.appendChild(document.createElement("p"));
     }
     audioDiv.style = ('position: fixed;top: 20%;right: 10px;');
     document.body.appendChild(audioDiv);
